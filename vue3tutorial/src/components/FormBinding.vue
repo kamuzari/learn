1810d27c872af3973e15c51a156b16bb937a38f8
whyWhale <soccer_by@naver.com>
2024-05-03 15:24:31 +0900

    tutorial: 폼 바인딩
    - v-bind, v-on으로 양방향 바인딩을 만들 수 있다.
    - 그 둘의 조합을 v-model 이라는 키워드를 사용하면 값을 바인딩 하고 상태와 자동으로 동기화 하므로 이벤트 헨들러를 사용할 필요가 없다.
    - v-model 체크박스, 라디오, 셀렉트 드롭다운과 같은 입력 타입에도 동작한다.

---
+++ vue3tutorial/src/components/FormBinding.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      combinatedText: '',
+      vmodelVar:''
+    }
+  },
+  methods: {
+    onInput(e) {
+      this.combinatedText = e.target.value
+    }
+  }
+}
+</script>
+
+<template>
+  <input :value="combinatedText" @input="onInput" placeholder="여기에 입력하기">
+  <p>{{ combinatedText }}</p>
+
+  <input v-model="vmodelVar">
+  <p>{{ vmodelVar}}</p>
+</template>
