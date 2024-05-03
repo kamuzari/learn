6ca8def6bb8940dee7710d63c7321de82e5b7ca8
whyWhale <soccer_by@naver.com>
2024-05-03 22:41:35 +0900

    tutorial: 컴포넌트
    - 컴포넌트 별 즉 모듈로 구별하여 구성한다면, 반복적으로 사용되는 코드 생산성을 향상시킬 수 있다
    - 해당 컴포넌트를 사용하겠다고 import 로 선언하고 export default 구문에 컴포넌트를 등록하면 template 내부에서 사용이 가능하다

--- vue3tutorial/src/App.vue
+++ vue3tutorial/src/App.vue
@@ -1,35 +1,14 @@
 <script>
+import childComponent from './components/newComponent.vue'
+
 export default {
-  data() {
-    return {
-      todoId: 1,
-      todoData: null
-    }
-  },
-  methods: {
-    async fetchData() {
-      console.log('fetch data....');
-      this.todoData = null
-      const res = await fetch(
-          `https://jsonplaceholder.typicode.com/todos/${this.todoId}`
-      )
-      this.todoData = await res.json()
-    }
-  },
- watch: {
-   todoId(newCount) {
-     this.fetchData();
-   }
- },
-  mounted() {
-    this.fetchData()
+  // 자식 컴포넌트를 등록
+  components: {
+    childComponent
   }
 }
 </script>
 
 <template>
-  <p>할 일 id: {{ todoId }}</p>
-  <button @click="todoId++">다음 할 일 가져오기</button>
-  <p v-if="!todoData">로딩...</p>
-  <pre v-else>{{ todoData }}</pre>
+  <childComponent/>
 </template>
---
+++ vue3tutorial/src/components/newComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1> newComponent 의 template 입니다!</h1>
+</template>
