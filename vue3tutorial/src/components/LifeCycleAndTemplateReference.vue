0c9d35a609ed5aa247b795ca5b3cf5596ffa706e
whyWhale <soccer_by@naver.com>
2024-05-03 16:41:46 +0900

    tutorial: 생명주기와 템플릿 참조
    - DOM 을 수동으로 작업해야 하는 경우가 있는데, ref 를 사용하여 템플릿 참조를 요청할 수 있다.
    - this.$refs.${ref 선언 변수} 를 사용하여 해당 html tag 로 접근할 수 있다.
    - 마운트 라는 키워드는 생명주기의 한 단계 이며 전반적인 라이프 사이클 바탕으로 다음 마운트 된 컴포넌트 를 알고 대응 할 수 있다.

---
+++ vue3tutorial/src/components/LifeCycleAndTemplateReference.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.p.textContent = '마운트 되었어요!'
+    this.$refs.ff.textContent = '마운트 되었어요!'
+  }
+}
+</script>
+
+<template>
+  <p ref="p">안녕</p>
+  <h1 ref="ff"> title </h1>
+</template>
