87ee342645b03e18dd2d938dfa6263149d7f1fe5
whyWhale <soccer_by@naver.com>
2024-05-03 15:19:53 +0900

    tutorial: 이벤트 리스너
    - v-on 디렉티브를 사용하여 DOM 이벤트 수신
    - this 키워드는 data 에 의해 선언된 데이터 속성을 노출한 곳에서만 사용가능

---
+++ vue3tutorial/src/components/EventListner.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- 이 버튼이 작동하도록 만들어 봅시다 -->
+  <button v-on:click="increment">"v-on:click='${method_name}' "숫자 세기: {{ count }}</button>
+  <h1><dasdas</h1>
+  <button @click="increment">"@click=${method_name}" 단축 문법 -> 숫자 세기: {{ count }}</button>
+</template>
