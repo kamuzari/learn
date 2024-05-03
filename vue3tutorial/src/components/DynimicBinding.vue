d9b0ed1d769192e4eedfc0ae23ba5e008e517353
whyWhale <soccer_by@naver.com>
2024-05-03 15:19:29 +0900

    tutorial: 속성 바인딩
    - mustache syntax 는 텍스트 삽입만 가능합니다.
    - 속성을 동적바인딩 하기 위해서느 v-bind 키워드를 사용합니다.
      - ex) : v-bind:${html tag property}="${선언한 속성}"

---
+++ vue3tutorial/src/components/DynimicBinding.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      titleClass: 'title',
+      vBindClass:'v-bind'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1 :class="titleClass">{ ':${html property}' }</h1>
+  <h1 v-bind:class="vBindClass"> ' v-bind:{html property}' </h1>
+</template>
+
+<style>
+.title{
+  color: deepskyblue;
+}
+
+.v-bind{
+  color: coral;
+}
+
+</style>
+
