711419945f7e5d59be07524a0717c9f909bf3f2b
whyWhale <soccer_by@naver.com>
2024-05-03 23:25:28 +0900

    tutorial: Emits
    - 자식 컴포넌트는 부모로 부터 이벤트를 발송(Emit) 할 수 있다
    - this.$emit ('${이벤트변수이름}','이벤트 발송 인자값 ...') 을 create() 함수로 선언해야한다
    - 부모는 v-on을 통해 이벤트를 수신할 수 있다 또한 자식이 이벤트를 발송할 떄 추가한 인자를 핸들러에서 받아 로컬 상태로 할당할 수도 있다

---
+++ vue3tutorial/src/components/emits/ChildComponent.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  emits: ['response'],
+  created() {
+    this.$emit('response', '자식 컴포넌트로부터 🌷를 받았어요!')
+  }
+}
+</script>
+
+<template>
+  <h2>자식 컴포넌트</h2>
+</template>
---
+++ vue3tutorial/src/components/emits/ParentComponent.vue
@@ -0,0 +1,19 @@
+<script>
+import ChildComp from './ChildComponent.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      childMsg: '자식 컴포넌트로부터 아직 메시지를 받지 못했어요!'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp  @response="(msg)=> childMsg=msg"/>
+  <p>{{ childMsg }}</p>
+</template>
