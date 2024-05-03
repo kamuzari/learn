909de4af2a28000fee2df17918b6cbef2123c927
whyWhale <soccer_by@naver.com>
2024-05-03 15:27:45 +0900

    tutorial: 조건부 렌더링

---
+++ vue3tutorial/src/components/ConditionRendering.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      awesome: true
+    }
+  },
+  methods: {
+    toggle() {
+      this.awesome = this.awesome ? false: true
+    }
+  }
+}
+</script>
+
+<template>
+  <button @click="toggle">토글 버튼</button>
+  <h1 v-if="awesome">Vue는 굉장해! 엄청나!</h1>
+  <h1 v-else>오 안돼 😢</h1>
+</template>
