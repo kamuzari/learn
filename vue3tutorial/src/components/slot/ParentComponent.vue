3000a974a5ac01aca62d01d4437109858b3f1753
whyWhale <soccer_by@naver.com>
2024-05-04 23:25:32 +0900

    tutorial: Slot
    - 부모 컴포넌트는 props를 통해 데이터를 전달할 수도 있으며 slot이라는 자식 컴포넌트의 템플릿을 부모 템플릿에서 전달할 수 도 있다.
    - 발산수단(slot)으로 사용하면 부모로부터의 템플릿 콘텐츠를 렌더링 할 수 있다

---
+++ vue3tutorial/src/components/slot/ParentComponent.vue
@@ -0,0 +1,18 @@
+<script>
+import ChildComp from './ChildComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      msg: 'Vue는 개발자에게 정말 유용하죠! 🎁'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp>부모 컨텐츠에서 자식에게 전달! ;)</ChildComp>
+</template>
