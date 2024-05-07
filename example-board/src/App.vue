2abb291ba1b39120932372a61ff41e08261183ed
whyWhale <soccer_by@naver.com>
2024-05-07 11:46:50 +0900

    init: vue3 게시판 만들어보기

---
+++ example-board/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import ChildComp from './components/ChildComp.vue'
+
+const childMsg = ref('자식 컴포넌트로부터 아직 메시지를 받지 못했어요!')
+</script>
+
+<template>
+  <ChildComp @d="(msg)=>childMsg=msg"/>
+  <p>{{ childMsg }}</p>
+  <p> dasndjanjksd</p>
+</template>
