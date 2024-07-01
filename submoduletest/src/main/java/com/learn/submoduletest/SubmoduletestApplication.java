b7e1e181c53fb90bc7cc528c296850be470d6b38
whyWhale <soccer_by@naver.com>
2024-07-01 13:02:37 +0900

    initial

---
+++ submoduletest/src/main/java/com/learn/submoduletest/SubmoduletestApplication.java
@@ -0,0 +1,13 @@
+package com.learn.submoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubmoduletestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubmoduletestApplication.class, args);
+    }
+
+}
---
+++ submoduletest/src/test/java/com/learn/submoduletest/SubmoduletestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.submoduletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmoduletestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
